a3d4327f039accdc863958bec7a8dc47f6f10483
Michael Lewis <mjolewis@gmail.com>
2020-07-28 21:02:03 -0400

    Fix: Add cost of carry
    -Cost of carry is a required parameter to price options

--- Input.cpp
+++ Input.cpp
@@ -33,16 +33,20 @@ namespace MJL {
          * @param sig Volatility
          * @param r Risk-free interest rate
          * @param S Spot price of the underlying
+         * @param K Strike price
+         * @param b Cost of carry
          * @throws OutOfMemoryException Indicates insufficient memory for this new Input object
          */
-        Input::Input(double &T_, double &sig_, double &r_, double &S_, double &K_) : T(T_), sig(sig_), r(r_), S(S_), K(K_) {}
+        Input::Input(double &T_, double &sig_, double &r_, double &S_, double &K_, double &b_) :
+                                                T(T_), sig(sig_), r(r_), S(S_), K(K_), b(b_) {}
 
         /**
          * Initialize a new Input object using the member data of the source
          * @param source An Input object whose data members will be used to initialize this Input objects members
          * @throws OutOfMemoryException Indicates insufficient memory for this new Input object
          */
-        Input::Input(const Input &source) : T(source.T), sig(source.sig), r(source.r), S(source.S), K(source.K) {}
+        Input::Input(const Input &source) : T(source.T), sig(source.sig), r(source.r),
+                                            S(source.S), K(source.K), b(source.b) {}
 
         /**
          * Destory's this Input object
@@ -63,6 +67,7 @@ namespace MJL {
             r = source.r;
             S = source.S;
             K = source.K;
+            b = source.b;
 
             return *this;
         }
@@ -154,17 +159,31 @@ namespace MJL {
                 // Handle input errors and crashes gracefully
                 if (K < 0 || !std::cin) {
 
-                    // Clear the error flat
+                    // Clear the error flag
                     std::cin.clear();
 
                     // Ignore input up to stream size or new line (whichever comes first)
                     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                     K = 65;                     // Page 36-37 of Intro to C++ for Financial Engineers by Dr. Duffy
                 }
+
+                std::cout << "Cost of carry: "; std::cin >> b;
+
+                // Handle input errors and crashes gracefully
+                if (b < 0 || !std::cin) {
+
+                    // Clear the error flag
+                    std::cin.clear();
+
+                    // Ignore input up to stream size or new line (whichever comes first)
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                    b = r;                       // Page 36-37 of Intro to C++ for Financial Engineers by Dr. Duffy
+                }
             } catch (std::exception& e) {
                 std::cout << e.what() << std::endl;
             }
-            optionData = boost::make_tuple(T, sig, r, S);
+
+            optionData = boost::make_tuple(T, sig, r, S, K, b);
             return optionData;
         }
     }
