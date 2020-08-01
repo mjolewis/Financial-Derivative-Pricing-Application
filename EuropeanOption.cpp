b66b305b538af7e68611455f2e115040053c8807
Michael Lewis <mjolewis@gmail.com>
2020-08-01 11:27:29 -0400

    Fix: Call base class constructor
    -Classes derived from Instrument need to call the Instrument constructors

--- EuropeanOption.cpp
+++ EuropeanOption.cpp
@@ -17,7 +17,7 @@ namespace MJL {
          * @throws OutOfMemoryError Indicates insufficient memory for this EuropeanOption
          */
         template<typename RNG_>
-        EuropeanOption<RNG_>::EuropeanOption() : T(0.25), sig(0.3), r(0.08), S(60), K(65), b(r), optType("Call") {}
+        EuropeanOption<RNG_>::EuropeanOption() : Instrument(), T(0.25), sig(0.3), r(0.08), S(60), K(65), b(r), optType("Call") {}
 
         /**
          * Initialize a new EuropeanOption with the source object
@@ -25,12 +25,14 @@ namespace MJL {
          * @throws OutOfMemoryError Indicates insufficient memory for this EuropeanOption
          */
         template<typename RNG_>
-        EuropeanOption<RNG_>::EuropeanOption(const EuropeanOption &source) : T(source.T), sig(source.sig), r(source.r),
-                                                    S(source.S), K(source.K), b(source.b), optType(source.optType){}
+        EuropeanOption<RNG_>::EuropeanOption(const EuropeanOption &source) : Instrument(), T(source.T),
+                                                                             sig(source.sig), r(source.r),
+                                                                             S(source.S), K(source.K),
+                                                                             b(source.b), optType(source.optType){}
 
         /**
          * Destroy this EuropeanOption
-         */
+         */git
         template<typename RNG_>
         EuropeanOption<RNG_>::~EuropeanOption() {}
 
@@ -43,6 +45,9 @@ namespace MJL {
         EuropeanOption<RNG_> & EuropeanOption<RNG_>::operator=(const EuropeanOption<RNG_> &source) {
             // Avoid self assign
             if (this == &source) { return *this; }
+
+            // Call base class constructor
+            Instrument::operator=(source);
 
             T = source.T;
             sig = source.sig;
